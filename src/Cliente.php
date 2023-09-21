3ec7fd09a45c94b09b28da8226cd9baba7a55291
Jhonatan Vinicius <jhonnytrabalho2005@gmail.com>
2023-09-21 11:24:32 -0300

    implementação dos tipos de dados na classe cliente

---
+++ src/Cliente.php
@@ -0,0 +1,11 @@
+<?php
+class Cliente {
+    // Propriedades (ou atributos)
+    public string $nome;
+    public string $email;
+    public string $senha;
+    public array $telefone;
+
+    
+}
+?>
