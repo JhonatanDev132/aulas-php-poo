3e867c1263d93da67c4b4e283fc3f8983d4153be
Jhonatan Vinicius <jhonnytrabalho2005@gmail.com>
2023-09-21 11:57:22 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,13 +6,13 @@
     <title>Exemplo1</title>
 </head>
 <body>
-    <h1>PHP com POO - Exemplo 1</h1>
+    <h1>PHP com POO - Exemplo 2</h1>
     <hr>
     <h2>Assuntos abordados:</h2>
     <ul>
-        <li>Criação de classe</li>
-        <li>Importação do arquivo de classe</li>
-        <li>Criação de instâncias/objetos</li>
+        <li>Acesso direto às propriedades</li>
+        <li>Atribuição e leitura de dados</li>
+        <li>Chamada de método</li>
     </ul>
 <?php
 // Importando a Classe
@@ -21,7 +21,23 @@ require_once "src/Cliente.php";
 // Criando instâncias da Classe (objetos!)
 $clienteA = new Cliente;
 $clienteB = new Cliente;
+
+// Acesso a atribuição
+$clienteA->nome = "Tiago";
+
+$clienteB->nome = "Jhonatan";
+
+$clienteA->telefone = ["4002-8922", "11-97217-0371"];
+
+$clienteA->email = "jhonatan@gmail.com";
+$clienteA->senha = password_hash("123abc", PASSWORD_DEFAULT);
 ?>
+
+<hr>
+
+<h2>Dados dos objetos</h2>
+<h3> <?= $clienteA->nome ?></h3>
+<h3> <?= $clienteB->nome ?></h3>
 
 <pre><?=var_dump($clienteA, $clienteB)?></pre>
 </body>
